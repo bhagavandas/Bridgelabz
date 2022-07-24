f6ef6a6d5ac9d6873c6591f16e912eb862f554b1
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-07-24 18:10:23 +0530

    Added: UC2

--- src/main/java/com/SnakeAndLadder/SnakeAndLadderUC1.java
+++ src/main/java/com/SnakeAndLadder/SnakeAndLadderUC1.java
@@ -1,11 +1,16 @@
 package com.SnakeAndLadder;
 
+import java.util.Random;
+
 public class SnakeAndLadderUC1 {
 	
 	public static void main(String[] args) {
 		
 		int position =0;
 		System.out.println("Position : " + position);
-	
+		
+		Random r = new Random();
+		int randomInt = r.nextInt(6) + 1;
+		System.out.println("Number: " + randomInt);
 	}
 }
