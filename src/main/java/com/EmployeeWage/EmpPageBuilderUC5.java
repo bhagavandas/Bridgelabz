0434131b0d3a28de97a496efd674ce8f41f53aaf
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-07-24 08:21:12 +0530

    Added: UC6

--- src/main/java/com/EmployeeWage/EmpPageBuilderUC5.java
+++ src/main/java/com/EmployeeWage/EmpPageBuilderUC5.java
@@ -5,27 +5,31 @@ public class EmpPageBuilderUC5 {
 	static final int IS_PART_TIME = 2;
 	static final int EMP_RATE_PER_HR = 20;
 	static final int No_of_Working_Days = 20;
+	static final int Max_Hrs_In_Month = 100;
 
 	public static void main(String[] args) {
+		int Total_emp_hrs = 0;
 		int emp_hrs = 0;
-		int empwage = 0;
-		int total_emp_wage = 0;
-		for (int day = 0; day < No_of_Working_Days; day++) {
-			int empAttendance = (int) Math.floor(Math.random() * 10 % 3);
-			switch (empAttendance) {
-			case IS_PART_TIME:
-				emp_hrs = 4;
-				break;
-			case IS_FULLTIME:
-				emp_hrs = 8;
-				break;
-			default:
-				emp_hrs = 0;
-			}
-			empwage = emp_hrs * EMP_RATE_PER_HR;
-			total_emp_wage += empwage;
-			System.out.println("Emp Wage : " + empwage);
+		int Total_Working_Days = 0;
+		while (Total_emp_hrs <= Max_Hrs_In_Month && Total_Working_Days <= No_of_Working_Days)
+			Total_Working_Days++; {
+		int empAttendance = (int) Math.floor(Math.random() * 10 % 3);
+		switch (empAttendance) {
+		case IS_PART_TIME:
+			emp_hrs = 4;
+			break;
+		case IS_FULLTIME:
+			emp_hrs = 8;
+			break;
+		default:
+			emp_hrs = 0;
 		}
-		System.out.println("Total Emp Wage : " + total_emp_wage);
+		Total_emp_hrs += emp_hrs;
+		System.out.println("Emp Days :" + Total_Working_Days + "  Emp Hours :" + emp_hrs);
 	}
+
+	int total_empwage = Total_emp_hrs * EMP_RATE_PER_HR;
+
+ System.out.println("Total Emp Wage : "+ total_empwage);
+ }
 }
