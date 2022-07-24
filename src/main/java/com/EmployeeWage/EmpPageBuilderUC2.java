3b67c943d980f4a07e37ffd27b2969689eda4c31
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-07-24 07:41:57 +0530

    Added:UC3

--- src/main/java/com/EmployeeWage/EmpPageBuilderUC2.java
+++ src/main/java/com/EmployeeWage/EmpPageBuilderUC2.java
@@ -4,17 +4,21 @@ public class EmpPageBuilderUC2 {
 	public static void main(String[] args) {
 		
 		int IS_FULLTIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HR = 20;
 		int emp_hrs=0;
 		int empwage =0;
 		
-		double empAttendance = Math.floor(Math.random()*10%2);
-		if(empAttendance == IS_FULLTIME)
+		double empAttendance = Math.floor(Math.random()*10%3);
+		if(empAttendance == IS_PART_TIME)
+			emp_hrs=4;
+		else 
+			if(empAttendance == IS_FULLTIME)
 			emp_hrs=8;
-		else
-			emp_hrs=0;
+			else
+				emp_hrs=0;
 		empwage = emp_hrs * EMP_RATE_PER_HR;
-			System.out.println("Daily Emp Wage : " + empwage);
+			System.out.println("Emp Wage : " + empwage);
 		
 		
 		
